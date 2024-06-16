b1151e0dbca3179924ab8359b2056083cf13ac2b
Davi Aguiar <daviagui13@gmail.com>
2024-06-16 14:43:51 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/cursoudemy/course/repositories/OrderItemRepository.java
+++ src/main/java/com/cursoudemy/course/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.cursoudemy.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cursoudemy.course.entities.OrderItem;
+import com.cursoudemy.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 
 }
